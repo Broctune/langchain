4e849646f704aeaad734f1fa5230b50d6be88e84
Dave Sjolin <dave.sjolin@gmail.com>
2023-08-09 20:15:35 -0700

    Added setup.py file necessary to install for editing with PIP

---
+++ libs/langchain/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='langchain',
+    version='0.1260',
+    packages=find_packages(),
+    install_requires=[
+        # List your dependencies here
+    ],
+    # Additional metadata can go here
+)
