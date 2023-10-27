90e55d9f22c49efd0de4cef0ea63531143f25ab5
Alex Larson <alex.m.larson@gmail.com>
2023-10-27 16:00:57 -0700

    upd setup version

--- libs/langchain/setup.py
+++ libs/langchain/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='langchain',
-    version='0.1260',
+    version='0.325',
     packages=find_packages(),
     install_requires=[
         # List your dependencies here
